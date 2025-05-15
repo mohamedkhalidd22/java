d764d536937cb4b37d37354c722d86655f85a982
mohamedkhalidd22 <mohamedkhalid23122000@gmail.com>
2025-05-15 19:35:36 +0300

    java1

---
+++ MyApp.java
@@ -0,0 +1,7 @@
+public class MyApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to my first Java project!");
+        System.out.println("I'm excited to learn Java and improve my programming skills.");
+        System.out.println("This is just the beginning, and the best is yet to come!");
+    }
+}

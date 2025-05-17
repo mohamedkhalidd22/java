81c3e2f9ea5cb10057531d011877804c9660877e
mohamedkhalidd22 <mohamedkhalid23122000@gmail.com>
2025-05-17 16:27:31 +0300

    added

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html; charset=UTF-8");
+        response.getWriter().println("<h1>مرحباً بك في تطبيق Java Web!</h1>");
+    }
+}
